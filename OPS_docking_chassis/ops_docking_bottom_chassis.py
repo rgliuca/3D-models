65d15b7689a600bf76284ff53e1e40d44b455ad9
Robert Liu <robert888@gmail.com>
2024-09-21 22:47:02 -0700

    Full cover

--- OPS_docking_chassis/ops_docking_bottom_chassis.py
+++ OPS_docking_chassis/ops_docking_bottom_chassis.py
@@ -33,13 +33,21 @@ pts = [
     (0, plate_length)
 ]
 
-holes = [
+holes = (
     ((30, 189), 3),
     ((110, 189), 3),
-    ((15, 70), 5),
-    ((125, 70), 5),
-    ((70, 20), 10)
-]
+)
+
+# format: ((x, y), height, hole dia, hole depth) 
+posts = (
+    ((15, 70), 6, 7, 3),
+    ((125, 70), 6, 7, 3),
+    ((70, 20), 6, 7, 2.5),
+    ((6, 8), 20, 8, 2.5),
+    ((6, 70), 20, 8, 2.5),
+    ((134, 70), 20, 8, 2.5),
+    ((134, 8), 20, 8, 2.5)
+)
 
 base_plate = (
     cq.Workplane("XY")
@@ -55,8 +63,33 @@ for each_hole in holes:
     dia = each_hole[1]
     base_plate = base_plate.moveTo(pos[0], pos[1]).hole(dia)
 
+for each_post in posts:
+    pos = each_post[0]
+    post_height = each_post[1]
+    post_hole_dia = each_post[2]
+    post_hole_depth = each_post[3]
+    base_plate = base_plate.moveTo(pos[0], pos[1]).circle(post_hole_dia / 2).extrude(post_height)
+
+base_plate = base_plate.faces(">Z").workplane()
+
+for each_post in posts:
+    pos = each_post[0]
+    post_height = each_post[1]
+    post_hole_dia = each_post[2]
+    post_hole_depth = each_post[3]
+    base_plate = base_plate.moveTo(pos[0], pos[1]).hole(post_hole_dia / 2)
 
 parts.append(base_plate)
+
+bracket = (
+    cq.Workplane("XY").
+    polyline(((22, 80), (22, 77), (118, 77), (118, 80))).close().extrude(8 + plate_height).
+    faces("<Y").workplane().moveTo(33, plate_height + 4).hole(3).
+    moveTo(107, plate_height + 4).hole(3)
+     
+)
+
+parts.append(bracket)
 
 result = cq.Workplane("XY")
 
